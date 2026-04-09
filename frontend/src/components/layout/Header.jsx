2efb4ec4418a98ba1f5ea48bcb434bf0c47a38fb
Lautaro Lombardo <lautilomba517@gmail.com>
2026-04-09 12:01:47 -0300

    cambios en header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,26 +1,35 @@
 import { projectName } from "../../data/data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Container from "./Container";
 import "./Header.css";
 
+function HeaderLinks() {
+  return (
+    <div className="header__links">
+      <a href="#servicios">Servicios</a>
+      <a href="#cobertura">Cobertura</a>
+      <a href="#paquetes">Galería</a>
+      <a href="#quienes-somos">Quienes somos</a>
+      <a href="#contacto" className="header__link__contacto">
+        <FontAwesomeIcon icon={faEnvelope} /> Solicitar un presupuesto
+      </a>
+    </div>
+  );
+}
+
 function Header() {
-    return (
-        <header className="header">
-            <Container>
-                <div className="header__name">
-                    <span>Mariano</span>
-                    <span>Eventos</span>
-                </div>
-                <div className="header__links">
-                    <a href="#servicios">Servicios</a>
-                    <a href="#paquetes">Galería</a>
-                    <a href="#quienes-somos">Quienes somos</a>
-                    <a href="#contacto" className="header__link__contacto">
-                        Solicitar un presupuesto
-                    </a>
-                </div>
-            </Container>
-        </header>
-    );
+  return (
+    <header className="header">
+      <Container>
+        <div className="header__name">
+          <span>Mariano</span>
+          <span>Eventos</span>
+        </div>
+        <HeaderLinks />
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
